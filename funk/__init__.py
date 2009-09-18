49d72126a4fd871f13aa8b724904c45e7f303b68
Michael Williamson <mike@zwobble.org>
2009-09-19 00:28:04 +0100

    Allow a method to expect no arguments

--- funk/__init__.py
+++ funk/__init__.py
@@ -11,7 +11,7 @@ class Fake(object):
         self._provides = {}
     
     def provides(self, method_name):
-        call = Call()
+        call = Call(method_name)
         self._provides[method_name] = call
         return call
     
@@ -27,10 +27,25 @@ class Fake(object):
 
 class Call(object):
     _return_value = None
+    _allowed_args = None
+    _allowed_kwargs = None
+    
+    def __init__(self, name):
+        self._name = name
     
     def __call__(self, *args, **kwargs):
-        return self._return_value
+        if self._allowed_args is not None and self._allowed_args != list(args) or self._allowed_kwargs is not None and self._allowed_kwargs != kwargs:
+            args_str = list(args[:])
+            args_str += ['%s=%s' % (key, kwargs[key]) for key in kwargs]
+            raise AssertionError("Unexpected method call: %s(%s)" % (self._name, ', '.join(args_str)))
         
+        return self._return_value
+    
+    def with_args(self):
+        self._allowed_args = []
+        self._allowed_kwargs = {}
+        return self
+    
     def returns(self, return_value):
         self._return_value = return_value
 
--- test/funk/test_funk.py
+++ test/funk/test_funk.py
@@ -34,9 +34,29 @@ def test_providing_a_method_without_specifying_arguments_allows_method_to_be_cal
     assert fake.save() is return_value
     assert fake.save(name="Bob") is return_value
 
+@funk.with_context
+def test_can_specify_no_arguments_when_using_provides(context):
+    return_value = "foo"
+    
+    fake = context.fake()
+    fake.provides('save').with_args().returns(return_value)
+    
+    assert fake.save() is return_value
+    assert_raises_str(AssertionError, "Unexpected method call: save(positional)", lambda: fake.save("positional"))
+    assert_raises_str(AssertionError, "Unexpected method call: save(key=word)", lambda: fake.save(key="word"))
+    assert_raises_str(AssertionError, "Unexpected method call: save(one, two, foo=bar, key=word)", lambda: fake.save("one", "two", key="word", foo="bar"))
+    assert fake.save() is return_value
+
 def test_calling_function_wrapped_in_with_context_raises_exception_if_context_already_set():
     @funk.with_context
     def some_function(context):
         pass
         
     assert_raises(FunkException, lambda: some_function(context=None))
+
+def assert_raises_str(exception, message, function):
+    try:
+        function()
+        raise AssertionException("%s was not raised" % exception)
+    except exception, e:
+        assert_equals(message, str(e))
