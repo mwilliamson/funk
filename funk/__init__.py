ff4029bacc14cd9c0c6d32b0941c48f275464cd2
Michael Williamson <mike@zwobble.org>
2009-09-18 23:17:21 +0100

    Raise exception if context has already been set

--- funk/__init__.py
+++ funk/__init__.py
@@ -7,7 +7,16 @@ class Context(object):
 def with_context(test_function):
     @wraps(test_function)
     def test_function_with_context(*args, **kwargs):
+        if 'context' in kwargs:
+            raise FunkException("context has already been set")
         kwargs['context'] = Context()
         test_function(*args, **kwargs)
     
     return test_function_with_context
+
+class FunkException(BaseException):
+    def __init__(self, message):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
--- test/funk/test_funk.py
+++ test/funk/test_funk.py
@@ -1,5 +1,15 @@
+from nose.tools import assert_raises
+
 import funk
+from funk import FunkException
 
 @funk.with_context
 def test_can_create_a_fake_object(context):
     fake = context.fake()
+
+def test_calling_function_wrapped_in_with_context_raises_exception_if_context_already_set():
+    @funk.with_context
+    def some_function(context):
+        pass
+        
+    assert_raises(FunkException, lambda: some_function(context=None))
