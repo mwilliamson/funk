656f724b3b9b6e51b217edbdca9d05d3ce2564c0
Michael Williamson <mike@zwobble.org>
2009-09-28 14:10:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Funk',
+    version='0.1',
+    description='A mocking framework for Python',
+    author='Michael Williamson',
+    author_email='mike@zwobble.org',
+    url='http://gitorious.org/funk',
+    packages=['funk'],
+)
