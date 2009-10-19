d2eb63d97df1549c12e246ffd1e1e7c31c398e10
Michael Williamson <mike@zwobble.org>
2009-10-19 15:44:03 +0100

    Bump version number to 0.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Funk',
-    version='0.1',
+    version='0.2',
     description='A mocking framework for Python',
     author='Michael Williamson',
     author_email='mike@zwobble.org',
