4e1ebd404bf29c172d995704bef16c71306512be
Michael Williamson <mike@zwobble.org>
2009-12-15 15:10:52 +0000

    Add ValueObject.__str__

--- funk/tools.py
+++ funk/tools.py
@@ -17,8 +17,15 @@ def assert_that(value, matcher):
 
 class ValueObject(object):
     def __init__(self, attributes):
+        self._keys = attributes.keys()
         for key in attributes:
             setattr(self, key, attributes[key])
+
+    def __str__(self):
+        attributes = {}
+        for key in self._keys:
+            attributes[key] = getattr(self, key)
+        return "<value_object: %s>" % attributes
 
 def value_object(**kwargs):
     return ValueObject(kwargs)
--- test/funk/test_tools.py
+++ test/funk/test_tools.py
@@ -71,3 +71,12 @@ def test_value_object_sets_attributes_to_passed_keyword_arguments():
     obj = value_object(width=20, height=40)
     assert_equals(obj.width, 20)
     assert_equals(obj.height, 40)
+
+def test_value_object_str_shows_attributes():
+    obj = value_object(width=20)
+    assert_equals(str(obj), "<value_object: {'width': 20}>")
+
+def test_value_object_str_shows_updated_attributes():
+    obj = value_object(width=20)
+    obj.width = 30
+    assert_equals(str(obj), "<value_object: {'width': 30}>")
