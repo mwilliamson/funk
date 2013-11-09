b901c17236a878d82cd5f83dd85aad71c6554c50
Michael Williamson <mike@zwobble.org>
2013-11-09 01:47:35 +0000

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Funk',
-    version='0.3',
+    version='0.3.1',
     description='A mocking framework for Python',
     author='Michael Williamson',
     author_email='mike@zwobble.org',
