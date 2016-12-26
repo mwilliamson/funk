14449f78027125f01c01bd360825dab2d1878bf9
Michael Williamson <mike@zwobble.org>
2016-12-26 12:20:35 +0000

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
 from setuptools import setup
+
+
+def read(path):
+    with open(os.path.join(os.path.dirname(__file__), path)) as fileobj:
+        return fileobj.read()
+
 
 setup(
     name='Funk',
     version='0.4.0',
     description='A mocking framework for Python',
+    long_description=read("README.rst"),
     author='Michael Williamson',
     author_email='mike@zwobble.org',
     url='http://github.com/mwilliamson/funk',
