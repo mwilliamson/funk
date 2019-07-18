93193a201e1c885b89b9eb25e32eefe6631aa9b1
Michael Williamson <mike@zwobble.org>
2019-07-18 19:07:39 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setup(
     packages=['funk'],
     keywords="mock",
     install_requires=["precisely>=0.1.1"],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license='BSD License'
 )
